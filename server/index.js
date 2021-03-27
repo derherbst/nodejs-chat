f988ce3209ff737780a1f335627a9718da70db1d
Georgii Gerbst <Georgii_Gerbst@epam.com>
2021-03-27 07:03:55 +0300

    #1: initial commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('New connection!');
+
+    socket.on('disconnect', () => {
+        console.log('User has left')
+    })
+})
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
